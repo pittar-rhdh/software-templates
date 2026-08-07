679c9462d4fdb34092ba4dd30c1cd6a9c4c93b43
Andrew Pitt <andrew.pitt@redhat.com>
2026-08-07 18:56:30 +0000

    Updated pipeline and catalog-info

---
+++ templates/java/quarkus/skeleton/source/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+-- Sample data for Fruit Entity
+-- This file will be automatically loaded by Dev Services when available
+
+-- Insert sample data with explicit ID values
+INSERT INTO fruits (id, name, colour) VALUES (1, 'Apple', 'Red');
+INSERT INTO fruits (id, name, colour) VALUES (2, 'Banana', 'Yellow');
+INSERT INTO fruits (id, name, colour) VALUES (3, 'Orange', 'Orange');
+INSERT INTO fruits (id, name, colour) VALUES (4, 'Grape', 'Purple');
+INSERT INTO fruits (id, name, colour) VALUES (5, 'Strawberry', 'Red');
